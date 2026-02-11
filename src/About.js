bd48b58b2e89bd6abe15a787306b07c193d2f7cf
deviceartist <deviceartist@sohu.com>
2026-02-11 11:42:17 +0800

    update

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ import {
 } from '@ant-design/icons';
 export const About = () => {
     return <>
-        <p>A screen designer for ssd1306 for microPython.</p>
+        <p>A screen designer</p>
         <Button icon={<GithubOutlined />} type="link" href="https://github.com/DeviceArtist/pixelStudio" target="_blank">Github</Button>
     </>
 }
